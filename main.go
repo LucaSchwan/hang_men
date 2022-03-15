ad5adc1255e7417c578dc759796265460769f26a
ehrenschwan <luca@ehrenschwan.com>
2022-03-15 17:53:56 +0100

    Fully implemented Hangman

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"math/rand"
 	"os"
@@ -31,7 +32,7 @@ func genWord() string {
 	return word
 }
 
-func gameloop(word string, foundLetter []byte, absentLetter []byte, lives int) ([]byte, []byte, int) {
+func gameloop(word string, foundLetters *list.List, guessedLetters *list.List, lives int) (int, bool, bool) {
 	hangmen := []string{`  +---+
   |   |
       |
@@ -88,11 +89,115 @@ func gameloop(word string, foundLetter []byte, absentLetter []byte, lives int) (
       |
 =========
 `}
+	fmt.Print(hangmen[len(hangmen)-1-lives])
+	fmt.Print("\n")
 
-	return []byte{}, []byte{}, 0
+	fmt.Printf("Lives: %d", lives)
+	fmt.Print("\n")
+
+	fmt.Print("Word: ")
+	for i := 0; i < len(word); i++ {
+		char := word[i]
+		if listContains(foundLetters, char) {
+			fmt.Printf("%c ", char)
+		} else {
+			fmt.Print("_ ")
+		}
+	}
+
+	fmt.Print("\n")
+
+	fmt.Print("Found Letters: ")
+	for foundLetter := foundLetters.Front(); foundLetter != nil; foundLetter = foundLetter.Next() {
+		fmt.Printf("%c ", foundLetter.Value)
+	}
+
+	fmt.Print("\n")
+
+	fmt.Print("Guessed Letters: ")
+	for guessedLetter := guessedLetters.Front(); guessedLetter != nil; guessedLetter = guessedLetter.Next() {
+		fmt.Printf("%c ", guessedLetter.Value)
+	}
+
+	fmt.Print("\n")
+
+	var letter byte
+	for letter < 97 || letter > 123 {
+		var input string
+		fmt.Print("Enter Letter: ")
+		fmt.Scanf("%s", &input)
+		letter = input[0]
+		if len(input) > 1 {
+			fmt.Println("You've inputed more than one letter")
+			letter = 0
+		}
+		if listContains(foundLetters, letter) {
+			fmt.Println("Letter was already found")
+			letter = 0
+		}
+		if listContains(guessedLetters, letter) {
+			fmt.Println("Letter was already guessed")
+			letter = 0
+		}
+	}
+
+	correct := false
+
+	for i := 0; i < len(word); i++ {
+		if letter == word[i] {
+			correct = true
+		}
+	}
+
+	if correct {
+		foundLetters.PushFront(letter)
+	} else {
+		guessedLetters.PushFront(letter)
+		lives -= 1
+	}
+
+	fmt.Print("\n")
+
+	done := false
+	if lives == 0 {
+		fmt.Print(hangmen[len(hangmen)-1-lives])
+		fmt.Print("\n")
+		fmt.Println("You've Lost!")
+		done = true
+	}
+
+	won := false
+	if len(word) == foundLetters.Len() {
+		won = true
+		done = true
+	}
+
+	return lives, done, won
+}
+
+func listContains(checkList *list.List, element interface{}) bool {
+	contains := false
+	for listElement := checkList.Front(); listElement != nil; listElement = listElement.Next() {
+		if element == listElement.Value {
+			contains = true
+		}
+	}
+	return contains
 }
 
 func main() {
-	genWord()
+	word := genWord()
 
+	foundLetters := list.New()
+	guessedLetters := list.New()
+	lives := 6
+	won := false
+	for done := false; !done; {
+		lives, done, won = gameloop(word, foundLetters, guessedLetters, lives)
+	}
+
+	if won {
+		fmt.Print("\n")
+		fmt.Println("You've Won!")
+	}
 }
